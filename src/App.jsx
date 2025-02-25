c0b0a0acd758c4921e09739d16c879372b6aec8f
Akash <thakare.akash007@gmail.com>
2025-02-25 16:11:02 +0530

    Implement routing and Navbar component for navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,24 @@
 
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import './App.css'
+import Navbar from './components/Navbar'
+import Home from './pages/Home'
+import CreatePost from './pages/CreatePost'
+import SinglePost from './pages/SinglePost'
 
 function App() {
 
 
   return (
     <>
-     <h1 class="text-3xl font-bold ">
-    Hello world!
-  </h1>
+     <Router>
+      <Navbar/>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/create' element={<CreatePost/>}/>
+        <Route path='/post/:id' element={<SinglePost/>}/>
+      </Routes>
+     </Router>
     </>
   )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <>
+      <nav className="bg-white shadow-lg">
+        <div className="max-w-7xl mx-auto px-4">
+          <div className="flex justify-between items-center h-16">
+            <Link to="/" className="text-2xl font-bold text-gray-800">
+              My Blog
+            </Link>
+            <div className="flex space-x-4">
+              <Link to="/" className="text-gray-600 hover:text-gray-900">
+                Home
+              </Link>
+              <Link to="/create" className="text-gray-600 hover:text-gray-900">
+                Create Post
+              </Link>
+            </div>
+          </div>
+        </div>
+      </nav>
+    </>
+  );
+};
 
-export default Navbar
+export default Navbar;
