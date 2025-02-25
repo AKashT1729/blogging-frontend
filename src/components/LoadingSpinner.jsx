91fea370cbc9df157c26019a61e583c11895936c
Akash <thakare.akash007@gmail.com>
2025-02-25 16:45:17 +0530

    Enhance LoadingSpinner component with improved styling and animation

--- src/components/LoadingSpinner.jsx
+++ src/components/LoadingSpinner.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import React from "react";
 
 const LoadingSpinner = () => {
   return (
-    <div>LoadingSpinner</div>
-  )
-}
+    <>
+      <div className="flex justify-center py-12">
+        <div className="animate-spin rounded-full h-12 w-12 border-4 border-gray-300 border-t-blue-600"></div>
+      </div>
+    </>
+  );
+};
 
-export default LoadingSpinner
+export default LoadingSpinner;
